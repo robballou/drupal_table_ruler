f3e02328afb0a1c4b6f235ba22ba493ee8b626ac
Rob Ballou <rob.ballou@gmail.com>
2013-06-12 09:17:08 -0600

    Initial commit

---
+++ js/ruler.js
@@ -0,0 +1,13 @@
+/**
+ * Admin table ruler
+ */
+(function($) {
+  $(document).ready(function() {
+    $('body.page-admin table').addClass('has-table-ruler');
+    $('body.page-admin tbody tr')
+    .click(function() {
+      $(this).toggleClass('has-table-ruler-highlight');
+    });
+  });
+
+})(jQuery);
